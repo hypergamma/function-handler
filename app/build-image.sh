991534f437ccd39e26681862e75dc9e9734d3c29
orsoblanc <iamorsoblanc@gmail.com>
2016-12-06 15:17:58 +0900

    code environment 정보 추가

--- app/build-image.sh
+++ app/build-image.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 
-REGISTRY="52.187.69.164:5000"
-IMAGE_NAME="event-handler"
+function usage() {
+	echo "./build_image.sh dockerfile_root env env_ver"
+}
+
+if [[ $# -ne 3 ]];
+then
+	usage;
+	exit -1; 
+fi
+
+dockerfile_root=$1
+env=$2
+env_ver=$3
+
+IMAGE_NAME="event-handler-"$env":"$env_ver
+
+
+
+if [[ $PRODUCTION_GAMMA = "production" ]];
+then
+    REGISTRY="52.187.69.164:5000"
+else
+    REGISTRY="0.0.0.0:5000"
+fi
+
+## change base
+sed "s/{{env}}/${env}/" $dockerfile_root/Dockerfile > $dockerfile_root/Dockerfile.1
+sed "s/{{env_ver}}/${env_ver}/" $dockerfile_root/Dockerfile.1 > $dockerfile_root/Dockerfile.final
+rm $dockerfile_root/Dockerfile.1
 
 # build image
-docker build -t ${REGISTRY}/$IMAGE_NAME $1
+docker build -t ${REGISTRY}/$IMAGE_NAME $dockerfile_root -f Dockerfile.final
 
 # check build success
 if [[ $? -ne 0 ]];
@@ -21,5 +48,6 @@ then
 	exit -2
 fi
 
+rm $dockerfile_root/Dockerfile.final
 
 exit 0 
